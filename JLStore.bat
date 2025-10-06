69ce15e9e75860680812906ee5fd91b6fa7709b9
Ort3nsia <antonelloisabella19@gmail.com>
2025-10-06 09:52:27 +0200

    Modifica Migration e File BAT

--- JLStore.bat
+++ JLStore.bat
@@ -1,11 +1,17 @@
+@echo off
+
+echo ----------Accetta certificati HTTPS----------
 dotnet dev-certs https --trust
 
+echo ----------Creazione Migrations----------
 mkdir "./JLStore/Migrations
 dotnet ef migrations add InitialCreate --project "./JLStore/JLStore.csproj"
 
+echo ----------Creazione DataBase----------
 mkdir "./JLStore/Data
 dotnet ef database update --project "./JLStore/JLStore.csproj"
 
+echo ----------Esecuzione Progetto----------
 dotnet run --project "./JLStore/JLStore.csproj"
 
 pause
